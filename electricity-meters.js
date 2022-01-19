5d7a9ad3fa5596a5ca75b1268bbedc89e7b726bc
nobu-hope <nobuhlelebz@gmail.com>
2022-01-19 13:17:39 +0200

    functions

--- electricity-meters.js
+++ electricity-meters.js
@@ -9,17 +9,20 @@ module.exports = function(pool) {
 
 	// for a given street show all the meters and their balances
 	function streetMeters(streetId) {
-
+		const meter = await pool.query('select street_number, balance from electricity_meter where street_id = $1', [streetId])
+			return meter.rows
 	}
 
 	// return all the appliances
 	function appliances() {
+		const appliances = await pool.query('select * from appliances');
+			return appliances.rows
 
 	}
 
 	// increase the meter balance for the meterId supplied
 	function topupElectricity(meterId, units) {
-
+		const topup = await pool.query('update electricity_meter set balance + $3 where ')
 	}
 	
 	// return the data for a given balance
